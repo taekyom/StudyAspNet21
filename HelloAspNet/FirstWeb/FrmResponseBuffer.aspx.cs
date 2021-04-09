2b062397a30fc214994b2636547e2bc1d4cf3c23
taekyom <tkyoung1014@naver.com>
2021-04-09 17:19:05 +0900

    210409 ASP.NET 첫 커밋

---
+++ HelloAspNet/FirstWeb/FrmResponseBuffer.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FirstWeb
+{
+    public partial class FrmResponseBuffer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Expires = -1; //현재페이지를 매번 새로 읽어옴
+            Response.Buffer = true; //buffer : 값을 담아놨다가 한번에 출력하는 것, 버퍼사용
+            Response.Write("[1] 현재 글이 보여짐");
+            Response.Flush(); //버퍼에 있는 내용 출력
+            Response.Write("[3] 현재 글은?");
+            Response.Clear(); //버퍼에 있는 내용 삭제
+            Response.Write("[4] 출력");
+            Response.End();
+            Response.Write("[5] 출력이 안됨");
+        }
+    }
+}
---
+++ HelloAspNet/FirstWeb/FrmResponseWrite.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.UI;
+
+namespace FirstWeb
+{
+    public partial class FrmResponseWrite : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("안녕하세요.<br/>");
+        }
+
+        protected void BtnMsg_Click(object sender, EventArgs e)
+        {
+            Response.Write("<span style='color:blue';>반갑습니다.</span>");
+        }
+
+        protected void BtnJs_Click(object sender, EventArgs e)
+        {
+            string strScript = @"<script language='javascript'>
+                                  window.alert('안녕하세요!');
+                                 </script>";
+            Response.Write(strScript);
+        }
+
+        protected void BtnLink_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("https://www.naver.com");
+        }
+    }
+}
