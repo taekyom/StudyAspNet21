bdc96be2ca5fcbba8a5b98a67a3e2767f5011182
taekyom <tkyoung1014@naver.com>
2021-04-14 15:18:59 +0900

    210414 회원가입 및 로그인 커밋

---
+++ HelloAspNet/LoginWebApp/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginWebApp.Models
+{
+    public class User
+    {
+        public int UID { get; set; }
+        public String UserID { get; set; }
+        public String Password { get; set; }
+    }
+}
