91b20ae24e53711fcbde7533d7550af5efeb5b8e
taekyom <tkyoung1014@naver.com>
2021-04-19 17:05:16 +0900

    210419 TODO리스트 커밋

---
+++ MVCSolutions/DbWebApp/Models/Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DbWebApp.Models
+{
+    public class Room
+    {
+        [Key]
+        public int RoomId { get; set; }
+        [Required] //not null
+        public string Name { get; set; }
+        public virtual ICollection<Speaker> Speakers { get; set; }
+    }
+    public class Speaker
+    {
+        [Key]
+        public int SpeakerId { get; set; }
+        [Required] //not null
+        public string Name { get; set; }
+        public int RoomId { get; set; }
+
+        public virtual Room room { get; set; }
+    }
+}
---
+++ MVCSolutions/TodoListWebApp/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoListWebApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) //이게 있어야 DB와 연동됨
+        {
+
+        }
+        
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
